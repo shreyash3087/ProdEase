acaedc49b18a73c303c979f704d9bd279aad27bd
Shreyash <shreyash3087@gmail.com>
2025-03-23 17:13:18 +0530

    fix: navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import "./globals.css";
-import Navbar from "@/components/navbar";
+import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 export default function RootLayout({ children }) {
   return (
